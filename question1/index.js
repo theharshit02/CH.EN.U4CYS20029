19aabf6c4f2e8577199b92897338655f87790c13
Harshit Mishra <harshitmishra2011@gmail.com>
2023-08-19 12:25:24 +0530

    ascending descending based on price and seat availability

--- question1/index.js
+++ question1/index.js
@@ -6,15 +6,45 @@ const request = require('request');
 const port = process.env.PORT || 3000
 const host = process.env.HOST || "localhost"
 
-const header = {
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTI0MjM0NzksImNvbXBhbnlOYW1lIjoiVHJhaW4gRGV0YWlscyIsImNsaWVudElEIjoiY2FjNjYzZjYtOGExMi00NjdmLWFhYzUtMDc4YTFlMDdkOGNiIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IkNILkVOLlU0Q1lTMjAwMjkifQ.e4R38xCGjVxPUR5F2Rc_jq50NbWWBdCBRdUiKvr6zY0'
-}
+const config = {
+    headers : {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTI0Mjc1MDIsImNvbXBhbnlOYW1lIjoiVHJhaW4gRGV0YWlscyIsImNsaWVudElEIjoiY2FjNjYzZjYtOGExMi00NjdmLWFhYzUtMDc4YTFlMDdkOGNiIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IkNILkVOLlU0Q1lTMjAwMjkifQ.NAsLMcCyevbgPxBEvycRqg89kEimVtBdvfmA5W6JKPI'
+    }
+  };
+
 
 app.get("/getTrain", function(req, res){
-    axios.get('http://20.244.56.144/train/trains', { header }).then(response=>{
-        console.log(response.data)
+    axios.get('http://20.244.56.144/train/trains', config ).then(response=>{
+        const data = response.data
+
+        sleeper = data.sort((a,b)=>{
+            if(a.price.sleeper < b.price.sleeper){
+                return -1
+            }
+        })
+
+        ac = data.sort((a,b)=>{
+            if(a.price.AC < b.price.AC){
+                return -1
+            }
+        })
+
+        acTix = data.sort((a,b)=>{
+            if(a.seatsAvailable.AC > b.seatsAvailable.AC){
+                return -1
+            }
+        })
+
+        sleeperTix = data.sort((a,b)=>{
+            if(a.seatsAvailable.sleeper > b.seatsAvailable.sleeper){
+                return -1
+            }
+        })
+    
+        res.send(acTix)
     })
-    res.send("hello")
+
+    
 })
 
 app.listen(port, function(err){
